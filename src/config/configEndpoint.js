4d6c60696fe230ce2733248671c85dcf8c39f2f8
gicquel <gicquel.pierreyves@gmail.com>
2016-12-14 12:23:40 +0100

    debug port issues

---
+++ src/config/configEndpoint.js
@@ -0,0 +1,2 @@
+var endpoint = 'http://ontologies.reveries-project.fr:3030/flore/sparql'
+module.export =endpoint
--- src/sparql/sparql.js
+++ src/sparql/sparql.js
@@ -1,11 +1,11 @@
 
 
 module.exports ={ 
+var endpoint=require('../config/configEndpoint.js') 	
+
 	getQueryResult : function(query,res) {
     var SparqlClient = require('sparql-client');
     var util = require('util');
-    var endpoint = 'http://ontologies.reveries-project.fr/$/flore/query'
- 	
     var client = new SparqlClient(endpoint);
     client.query(query)
         .execute(function(error, results) {
